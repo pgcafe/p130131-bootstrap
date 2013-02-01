7fd037bc33a0a9e1422a235d1c038a59fc17e0dd
Tatsuro Ueda <ueda_tatsuro@yahoo.co.jp>
2013-02-01 13:04:19 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P130131Bootstrap::Application.config.secret_token = 'e39dd28837a7bf66babfebf54860d13e8659518ef693a387211d9cb2afb84c4d954bc9f282dd67923c9bf09252ee9479f0dce42d3aa6dd4a5c7923ff8e104f92'
